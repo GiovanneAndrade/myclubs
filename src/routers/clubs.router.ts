a8b989fd4b2bba32e35c1338f2ac47484237d030
GiovanneAndrade <vanne.as@hotmail.com>
2023-01-29 19:13:44 -0300

    :sparkles:	feat:   clubs routers

--- src/routers/clubs.router.ts
+++ src/routers/clubs.router.ts
@@ -1,13 +1,23 @@
-import { Router } from "express"
-import { getClubsController, postClubsController } from "../controllers/clubs.controller.js"
+import { Router } from "express";
+import {
+  deleteClubController,
+  getClubsController,   
+  getStadiunController,             
+  postClubsController,
+  postStadiunController,
+  updateClubController,
+} from "../controllers/clubs.controller.js";
+import postClubsSchema from "../schemas/clubs.schemas.js";
+import { validateSchema } from "../schemas/shemaValidation.js";
+
+const router = Router();
+
+router.get("/clubs", getClubsController);
+router.post("/clubs", validateSchema(postClubsSchema), postClubsController);
+router.delete("/clubs/:id", deleteClubController);
+router.put("/clubs/:id", updateClubController);
  
- 
-    
+router.post("/stadium", postStadiunController);
+router.get("/stadium", getStadiunController);
 
-const router = Router()
-
-router.get('/clubs', getClubsController)
-router.post('/clubs', postClubsController)
-
-
-export default router
+export default router;
