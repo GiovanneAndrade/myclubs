8d56c38b5e58e5368ea33bce65f0611e69b9676a
GiovanneAndrade <vanne.as@hotmail.com>
2023-01-29 19:13:30 -0300

    :sparkles:	feat:   fans routers

--- src/routers/fans.router.ts
+++ src/routers/fans.router.ts
@@ -1,5 +1,7 @@
 import { Router } from "express"
 import { getFansController, postFansController } from "../controllers/fans.controller.js"
+import postFansSchema from "../schemas/fans.schemas.js"
+import { validateSchema } from "../schemas/shemaValidation.js"
  
 
 
