3e449bb750c483c746cf3bef3a6e8fdc3e48a096
GiovanneAndrade <vanne.as@hotmail.com>
2023-01-29 19:14:42 -0300

    :sparkles:	feat:   clubs repositories

--- src/repositores/clubs.repository.ts
+++ src/repositores/clubs.repository.ts
@@ -18,21 +18,95 @@ function getClubsRepository() {
   return result;
 }
 
-function postClubsRepository() {
-  const result = prisma.clubs.create({
+function postStadiunRepository(name: string, id: number) {
+  const result = prisma.stadium.create({
     data: {
-      name: "Figueirense",
-      city: "Santa Catarina",
-      colors: "Preto e Branco",
-      country: "Brasil",
-      pais: "",
-      championship: "",
-      championshipId: 4,
-      
+      name: name,
+      club: {
+        connect: {
+          id: Number(id),
+        },
+      },
+    },
+  });
+
+  return result;
+}
+function getStadiunRepository() {
+  const result = prisma.stadium.findMany({
+    select: {
+      id: true,
+      name: true,
+      club: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
     },
   });
 
   return result;
 }
 
-export { getClubsRepository, postClubsRepository };
+function postClubsRepository({
+  name,
+  city,
+  colors,
+  country,
+  championshipId: number,
+}) {
+  const result = prisma.clubs.create({
+    data: {
+      name: name,
+      city: city,
+      colors: colors,
+      country: country,
+      championshipId: number,
+    },
+  });
+
+  return result;
+}
+
+function deleteClubRepository(id: number) {
+  const result = prisma.clubs.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  return result;
+}
+function updateClubRepository({
+  name,
+  city,
+  colors,
+  country,
+  championshipId,
+  id,
+}) {
+  const result = prisma.clubs.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      name: name,
+      city: city,
+      colors: colors,
+      championshipId: championshipId,
+      country: country,
+    },
+  });
+
+  return result;
+}
+
+export {
+  getClubsRepository,
+  postClubsRepository,
+  postStadiunRepository,
+  getStadiunRepository,
+  deleteClubRepository,
+  updateClubRepository,
+};
